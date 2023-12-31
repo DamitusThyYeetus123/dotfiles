2aacfe8e8d855113ff475aefc696f1837678b22f
DamitusThyYeetus123 <deanam1507@gmail.com>
2023-12-31 19:05:58 +1100

    Add nvim-tree on startup

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -27,3 +27,7 @@ require("lazy").setup({{ "catppuccin/nvim", name = "catppuccin", priority = 1000
 vim.cmd.colorscheme "catppuccin-mocha"
 
 require("nvim-tree").setup()
+local function open_nvim_tree()
+  require("nvim-tree.api").tree.open()
+end
+vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
