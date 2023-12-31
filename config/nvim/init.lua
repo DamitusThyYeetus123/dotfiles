4baa57bfda24f24052331425d51250cd9c3ca2dc
DamitusThyYeetus123 <deanam1507@gmail.com>
2023-12-31 18:59:59 +1100

    Add neovim configs

---
+++ config/nvim/init.lua
@@ -0,0 +1,29 @@
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+require("lazy").setup({{ "catppuccin/nvim", name = "catppuccin", priority = 1000 },{
+  "nvim-tree/nvim-tree.lua",
+  version = "*",
+  lazy = false,
+  dependencies = {
+    "nvim-tree/nvim-web-devicons",
+  },
+  config = function()
+    require("nvim-tree").setup {}
+  end,
+}})
+vim.cmd.colorscheme "catppuccin-mocha"
+
+require("nvim-tree").setup()
